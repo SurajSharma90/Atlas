48f928c097b271abc5e5a30bd77d42e7ff2d3166
Suraj Sharma <suraj.sharma@tobii.com>
2023-11-17 13:39:49 +0100

    Add functionality to add point to vector

--- Python/Learning/PyGame/VectorMath/VectorMath.py
+++ Python/Learning/PyGame/VectorMath/VectorMath.py
@@ -9,14 +9,22 @@ class Vector2f:
 
 class Vectorf:
     def __init__(self, x: float, y: float):
-        self.x = x
-        self.y = y
-        self.magnitutde = sqrt(pow(self.x, 2) + pow(self.y, 2))
+            self.x = x
+            self.y = y
+            self.magnitutde = sqrt(pow(self.x, 2) + pow(self.y, 2))
 
     def __init__(self, a: Vector2f, b: Vector2f):
         self.x = b.x - a.x
         self.y = b.y - a.y
         self.magnitutde = sqrt(pow(self.x, 2) + pow(self.y, 2))
 
-    def normalize():
-        return self.__init__()
+    """
+    def __add__(self, other):
+        return Vectorf(Vector2f(self.x + other.x), self.y + other.y)
+    """
+
+    def normalized(self):
+        return Vectorf(self.x / self.magnitutde, self.y / self.magnitutde)
+    
+    def mean_point(self, a:Vector2f, b:Vector2f):
+        return Vector2f((a.x + b.x)/2.0, (a.y + b.y)/2.0)
--- Python/Learning/PyGame/VectorMath/__main__.py
+++ Python/Learning/PyGame/VectorMath/__main__.py
@@ -5,6 +5,8 @@ from VectorMath import Vectorf, Vector2f
 pygame.init()
 
 SCREEN_DIMENSIONS = {"x": 1920, "y": 1080}
+POINT_RADIUS = 10
+LABEL_RADIUS = 5
 
 display = pygame.display
 screen = display.set_mode((SCREEN_DIMENSIONS["x"], SCREEN_DIMENSIONS["y"]))
@@ -12,7 +14,12 @@ display.set_caption("Simple Window")
 
 points: [Circle] = []
 labels: [Circle] = []
-points.append(Circle(Vector2f(50.0, 50.0), 20, pygame.Color(255, 0, 0, 255)))
+points.append(Circle(Vector2f(50.0, 50.0), POINT_RADIUS, pygame.Color(255, 0, 0, 255)))
+points.append(Circle(Vector2f(120.0, 120.0), POINT_RADIUS, pygame.Color(255, 0, 0, 255)))
+
+vectorab=Vectorf(points[0].point, points[1].point)
+
+labels.append(Circle(Vector2f(vectorab.x, vectorab.y), POINT_RADIUS, pygame.Color(0, 0, 255, 255)))
 create = True
 
 running = True
@@ -49,6 +56,8 @@ while running:
 
     for point in points:
         point.render(screen)
+    for label in labels:
+        label.render(screen)
 
     display.update()
 
