c8adec1d9263237b641e66c1cc64d79b4af4215c
Suraj Sharma <suraj.sharma@tobii.com>
2023-11-30 15:37:49 +0100

    Functions for dict

--- Python/Learning/Console/Dictionary.py
+++ Python/Learning/Console/Dictionary.py
@@ -16,3 +16,39 @@ print(d)
 d.clear()
 
 print(d)
+
+#Set default value for undefined key
+d = {}
+d.setdefault("first", 2)
+print(d["first"])
+
+#Copy (shallow)
+d1 = {"First": 1}
+d2 = d1.copy() #Shallow copy! Need to use deepcopy to really make a new dict
+print(d1, d2)
+
+#fromkeys
+d1 = {"First": 1, "Second": 2}
+d2 = d1.fromkeys(("First", "Second"), 3)
+print(d1, d2)
+
+#items
+d1 = {"First": 1, "Second": 2}
+print(d1.items())
+
+#values
+d1 = {"First": 1, "Second": 2}
+print(d1.values())
+
+#update
+d1 = {"First": 1, "Second": 2}
+d2 = {"Third": 3, "Fourth": 4}
+print(d1, d2)
+d1.update(d2)
+print(d1, d2)
+
+if "Fourth" in d1:
+    print("Key Fourth is in dictionary d1")
+
+if "Fifth" not in d1:
+    print("Key Fifth is NOT in dictionary d1")
