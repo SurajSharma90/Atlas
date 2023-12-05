db9566e1118a445587c145a20a329dc90e22af98
Suraj Sharma <suraj.sharma@tobii.com>
2023-12-05 15:39:30 +0100

    First solution

---
+++ AdventOfCode/2023/1/solution.py
@@ -0,0 +1,15 @@
+try:
+    file = open("input.txt", "r")
+
+    sum = 0
+    for line in file:
+        for s in line:
+            if s.isdigit():
+                sum += int(s)
+    
+    print(sum)
+
+except Exception as e:
+    print(e)
+
+
