d8f76318dbe389cfcf84da0f86a024703422604d
Suraj Sharma <suraj-sharma@live.com>
2024-03-02 20:18:52 +0100

    Example code for input

--- CPP/ByteSized/byteSized_01-InputOutput.cpp
+++ CPP/ByteSized/byteSized_01-InputOutput.cpp
@@ -2,6 +2,13 @@
 
 int main(int argc, char** argv) {
   
+  std::cout << "Hello World!";
+
+  std::cout << "Hello" << "World!";
+
+  std::cout << "New Line!" << "\n";
   
+  std::cout << "End the line!" << std::endl;
+
   return 0;
 }
