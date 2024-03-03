490b85e7d162e3c4454be72fa3d74aef0716f03b
Suraj Sharma <suraj-sharma@live.com>
2024-03-03 22:53:39 +0100

    Initial add of all files for basic cpp

--- Udemy/Console_RPG_Game/Basic_CPP/02_types_and_variables.cpp
+++ Udemy/Console_RPG_Game/Basic_CPP/02_types_and_variables.cpp
@@ -1,26 +1,40 @@
-/*
-* Include the library that contains the functionality for input and output
-* iostream stands for input/output stream
-* Allows us to implement input and output in our application
-*/
-#include <iostream>
-
-//Namespace to define what part of the library we want to get the functionality from
-using namespace std;
-
-//Main function
-int main(int argc, char** argv)
-{
-  //Number types
-  int myInt = 0;
-  
-  //Decimal types
-  float myFloat = 1.0;
-  double myDouble = 1.1;
-
-  //Text and characters
-
-
-  return 0;
-}
-
+///*
+//* Include the library that contains the functionality for input and output
+//* iostream stands for input/output stream
+//* Allows us to implement input and output in our application
+//*/
+//#include <iostream>
+//
+////Namespace to define what part of the library we want to get the functionality from
+//using namespace std;
+//
+////Main function
+//int main(int argc, char** argv)
+//{
+//  //Integer types
+//  int myInt = 0;
+//  
+//  //Decimal types
+//  //Decimal types
+//  float myFloat = 1.0;
+//  double myDouble = 1.1;
+//
+//  //Text and characters
+//  char myChar = 'c';
+//  std::string myString = "Hello World!";
+//
+//  //Logical types
+//  bool doYouEvenLift = false;
+//  bool doYouEvenLiftBro = 0;
+//
+//  //Additional keywords
+//  short int myShort = 0;
+//  long int myLong = 0;
+//  unsigned int myUnsigned = 0;
+//
+//  //Const
+//  const int i = 23;
+//
+//  return 0;
+//}
+//
