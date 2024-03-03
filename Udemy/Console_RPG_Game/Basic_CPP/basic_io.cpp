11e4eeccab597e1083981ada2dd82f8ee0d094e0
Suraj Sharma <suraj-sharma@live.com>
2024-03-03 09:40:31 +0100

    Add udemy courses folder

---
+++ Udemy/Console_RPG_Game/Basic_CPP/basic_io.cpp
@@ -0,0 +1,14 @@
+/*
+* Include the library that contains the functionality for input and output
+* iostream stands for input/output stream
+* Allows us to implement input and output in our application
+*/
+#include <iostream>
+
+//Main function without arguments
+int main(int)
+{
+  
+  return 0;
+}
+
