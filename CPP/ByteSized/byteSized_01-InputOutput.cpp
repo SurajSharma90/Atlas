581ac6f617454634bb79298f701a95c70fa5ccd4
Suraj Sharma <suraj-sharma@live.com>
2024-03-03 10:23:33 +0100

    Updated io bytesized

--- CPP/ByteSized/byteSized_01-InputOutput.cpp
+++ CPP/ByteSized/byteSized_01-InputOutput.cpp
@@ -6,9 +6,17 @@ int main(int argc, char** argv) {
 
   std::cout << "Hello" << "World!";
 
-  std::cout << "New Line!" << "\n";
-  
-  std::cout << "End the line!" << std::endl;
+  std::cout << "New Line!" << std::endl;
+
+  std::cout << "Escape Character New Line!" << "\n";
+
+  std::cout << "Combine" 
+  << "\nSome" 
+  << "\n" 
+  << "Stuff\n"
+  << 34
+  << "\nis\n"
+  << "my age\n";
 
   return 0;
 }
