0e156121f706ab12136fa4490939738b43c2ce81
Suraj Sharma <suraj-sharma@live.com>
2024-03-03 10:00:39 +0100

    io complete

--- Udemy/Console_RPG_Game/Basic_CPP/basic_io.cpp
+++ Udemy/Console_RPG_Game/Basic_CPP/basic_io.cpp
@@ -5,10 +5,31 @@
 */
 #include <iostream>
 
-//Main function without arguments
+//Namespace to define what part of the library we want to get the functionality from
+//using namespace std;
+
+//Main function
 int main(int)
 {
-  
+  //Hello World!
+  std::cout << "Hello World!" << std::endl;
+
+  //Stream of strings
+  std::cout << "Hello" << " " << "World" << std::endl;
+
+  //Stream of strings on different lines
+  std::cout 
+  << "Hello" 
+  << " " 
+  << "World" 
+  << std::endl;
+
+  //Stream of strings on actual different lines
+  std::cout << "Hello\n" << " \n" << "World" << std::endl;
+
+  //Numbers and other types of data in the stream
+  std::cout << "My Number is: " << 25 << "\n";
+
   return 0;
 }
 
