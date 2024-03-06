dde68e7296fc79a91d2258a654786d4cb45b8eaa
Suraj Sharma <suraj-sharma@live.com>
2024-03-06 22:32:55 +0100

    Updating darray and adding basic input file

--- Udemy/Console_RPG_Game/Basic_CPP/03_basic_input.cpp
+++ Udemy/Console_RPG_Game/Basic_CPP/03_basic_input.cpp
@@ -4,6 +4,8 @@
 * Allows us to implement input and output in our application
 */
 #include <iostream>
+#include <string>
+
 
 //Namespace to define what part of the library we want to get the functionality from
 using namespace std;
@@ -11,29 +13,45 @@ using namespace std;
 //Main function
 int main(int argc, char** argv)
 {
-  //Integer types
-  int myInt = 0;
-  
-  //Decimal types
-  float myFloat = 1.0;
-  double myDouble = 1.1;
+  int myAge = 0;
+  std::string myName = "";
 
-  //Text and characters
-  char myChar = 'c';
-  std::string = "Hello World!";
+  std::cout << "Input your age please: ";
+  std::cin >> myAge;
 
-  //Logical types
-  bool doYouEvenLift = false;
-  bool doYouEvenLiftBro = 0;
+  std::cout << "\n";
 
-  //Additional keywords
-  short int myShort = 0;
-  long int myLong = 0;
-  unsigned int myUnsigned = 0;
+  std::cout << "Input your name please: ";
+  std::cin >> myName;
 
-  //Const
-  const int i = 23;
+  std::cout << "\n";
+
+  std::cout << "Your name is " << myName << " and you are " << myAge << " years old!";
+
+  std::cout << "\n";
+
+  //Only one part of the name is shown because the string is cut off because of the white space.
+  std::cout << "Input your name please: ";
+  std::getline(std::cin, myName);
+
+  std::cout << "\n";
+
+  std::cout << "Your name is " << myName << " and you are " << myAge << " years old!";
+
+  //Cin still had the newline character in the stream and instantly closes getline
+  //Flush the stream to use getline again
+
+  std::cin.ignore(INT_MAX);
+  std::cin.clear();
+
+  std::cout << "\n";
+
+  std::cout << "Input your name please: ";
+  std::getline(std::cin, myName);
+
+  std::cout << "\n";
+
+  std::cout << "Your name is " << myName << " and you are " << myAge << " years old!";
 
   return 0;
 }
-
--- Udemy/Console_RPG_Game/DArray/DArray_Test.cpp
+++ Udemy/Console_RPG_Game/DArray/DArray_Test.cpp
@@ -1,41 +1,41 @@
-/*
-* Include the library that contains the functionality for input and output
-* iostream stands for input/output stream
-* Allows us to implement input and output in our application
-*/
-#include <iostream>
-#include "DArray.h"
-
-//Namespace to define what part of the library we want to get the functionality from
-//using namespace std;
-
-//Main function
-int main(int argc, char** argv)
-{
-  DArray<int> myArr;
-  std::cout << "Nr of items: " << myArr.getNrOfItems() << "\n";
-
-  for (size_t i = 0; i < 11; i++)
-  {
-    myArr.add(i);
-  }
-  std::cout << myArr.getNrOfItems() << "\n";
-
-  for (size_t i = 0; i < myArr.getNrOfItems(); i++)
-  {
-    std::cout << i << ": " << myArr.at(i) << "\n";
-  }
-  std::cout << "Nr of items: " << myArr.getNrOfItems() << "\n";
-
-  myArr.remove(0);
-  myArr.remove(4);
-  myArr.remove(myArr.getNrOfItems()-1);
-  
-  for (size_t i = 0; i < myArr.getNrOfItems(); i++)
-  {
-    std::cout << i << ": " << myArr.at(i) << "\n";
-  }
-  std::cout << "Nr of items: " << myArr.getNrOfItems() << "\n";
-
-  return 0;
-}
+///*
+//* Include the library that contains the functionality for input and output
+//* iostream stands for input/output stream
+//* Allows us to implement input and output in our application
+//*/
+//#include <iostream>
+//#include "DArray.h"
+//
+////Namespace to define what part of the library we want to get the functionality from
+////using namespace std;
+//
+////Main function
+//int main(int argc, char** argv)
+//{
+//  DArray<int> myArr;
+//  std::cout << "Nr of items: " << myArr.getNrOfItems() << "\n";
+//
+//  for (size_t i = 0; i < 11; i++)
+//  {
+//    myArr.add(i);
+//  }
+//  std::cout << myArr.getNrOfItems() << "\n";
+//
+//  for (size_t i = 0; i < myArr.getNrOfItems(); i++)
+//  {
+//    std::cout << i << ": " << myArr.at(i) << "\n";
+//  }
+//  std::cout << "Nr of items: " << myArr.getNrOfItems() << "\n";
+//
+//  myArr.remove(0);
+//  myArr.remove(4);
+//  myArr.remove(myArr.getNrOfItems()-1);
+//  
+//  for (size_t i = 0; i < myArr.getNrOfItems(); i++)
+//  {
+//    std::cout << i << ": " << myArr.at(i) << "\n";
+//  }
+//  std::cout << "Nr of items: " << myArr.getNrOfItems() << "\n";
+//
+//  return 0;
+//}
