9741363a8bbc8164fb5c8f6eae37b56df39475e2
Suraj Sharma <suraj-sharma@live.com>
2024-03-08 22:38:42 +0100

    Start of iomanip

--- Udemy/Console_RPG_Game/Basic_CPP/04_iomanip.cpp
+++ Udemy/Console_RPG_Game/Basic_CPP/04_iomanip.cpp
@@ -4,6 +4,7 @@
 * Allows us to implement input and output in our application
 */
 #include <iostream>
+#include <iomanip>
 
 //Namespace to define what part of the library we want to get the functionality from
 using namespace std;
@@ -11,6 +12,21 @@ using namespace std;
 //Main function
 int main(int argc, char** argv)
 {
+  /*
+    get_money	Obtains a monetary amount, optionally in international format.
+    get_time	Obtains a time in a time structure by using a specified format.
+    put_money	Provides a monetary amount, optionally in international format.
+    put_time	Provides a time in a time structure and a format string to use.
+    quoted	Enables convenient round-tripping of strings with insertion and extraction operators.
+    resetiosflags	Clears the specified flags.
+    setbase	Set base for integers.
+    setfill	Sets the character that will be used to fill spaces in a right-justified display.
+    setiosflags	Sets the specified flags.
+    setprecision	Sets the precision for floating-point values.
+    setw	Specifies the width of the display field.
+  */
+
+  
 
   return 0;
 }
