12bbaaa1a1faac863dab7198660f2892fb1c245b
Suraj Sharma <suraj-sharma@live.com>
2024-03-13 22:26:35 +0100

    Unity 2d game video 7

--- Unity/2DYTTutorial/Assets/Scripts/Movement/MovementController.cs
+++ Unity/2DYTTutorial/Assets/Scripts/Movement/MovementController.cs
@@ -6,6 +6,13 @@ public class MovementController : MonoBehaviour
 {
     [SerializeField]
     Rigidbody2D playerRigidbody2D;
+    [SerializeField]
+    private float jumpForce = 12f;
+    [SerializeField]
+    private Transform bottomCollisionPoint = null;
+    private bool isGround = true;
+    [SerializeField]
+    private LayerMask tileLayer;
 
     [SerializeField]
     private float maxVelocity = 5f;
@@ -24,6 +31,17 @@ public class MovementController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGround = Physics2D.OverlapCircle(bottomCollisionPoint.position, .2f, tileLayer);
+
+        if(Input.GetButtonDown("Jump") && isGround == true) 
+        {
+            isGround = false;
+            playerRigidbody2D.velocity = new Vector2(
+                playerRigidbody2D.velocity.x, 
+                jumpForce
+                );
+        }
+        
         playerRigidbody2D.velocity = new Vector2(
             Input.GetAxis("Horizontal") * maxVelocity, 
             playerRigidbody2D.velocity.y
