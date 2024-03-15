234515b63399a752c7b52a3457cff147a257848a
Suraj Sharma <suraj-sharma@live.com>
2024-03-15 22:26:49 +0100

    Unity video 8

--- Unity/2DYTTutorial/Assets/Scripts/Movement/MovementController.cs
+++ Unity/2DYTTutorial/Assets/Scripts/Movement/MovementController.cs
@@ -4,22 +4,40 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField]
     Rigidbody2D playerRigidbody2D;
     [SerializeField]
-    private float jumpForce = 12f;
+    private float maxVelocity = 5f;
+    [Space(10)]
+
+    [Header("Jumping")]
     [SerializeField]
     private Transform bottomCollisionPoint = null;
-    private bool isGround = true;
     [SerializeField]
     private LayerMask tileLayer;
-
     [SerializeField]
-    private float maxVelocity = 5f;
+    private float jumpForce = 12f;
+    [SerializeField]
+    private int maxJumpCount = 1;
+    [SerializeField]
+    private int jumpCount = 0;
+    private bool isGround = true;
+    [Space(10)]
+
+    [Header("Animation")]
+    [SerializeField]
+    SpriteRenderer playerSpriteRenderer = null;
+    [Space(10)]
+
+    [Header("Camera")]
+    [SerializeField]
+    private Transform cameraTransform = null;
 
     private void Awake() 
     {
         playerRigidbody2D = GetComponent<Rigidbody2D>();
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Start is called before the first frame update
@@ -31,10 +49,9 @@ public class MovementController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        isGround = Physics2D.OverlapCircle(bottomCollisionPoint.position, .2f, tileLayer);
-
-        if(Input.GetButtonDown("Jump") && isGround == true) 
+        if(Input.GetButtonDown("Jump") && jumpCount < maxJumpCount) 
         {
+            jumpCount++;
             isGround = false;
             playerRigidbody2D.velocity = new Vector2(
                 playerRigidbody2D.velocity.x, 
@@ -42,14 +59,44 @@ public class MovementController : MonoBehaviour
                 );
         }
         
+        if(isGround == true && jumpCount > 0) 
+            jumpCount = 0;
+
         playerRigidbody2D.velocity = new Vector2(
             Input.GetAxis("Horizontal") * maxVelocity, 
             playerRigidbody2D.velocity.y
             );
     }
 
+    private void LateUpdate() 
+    {
+        if(playerRigidbody2D.velocity.x < 0f)
+            playerSpriteRenderer.flipX = true;
+        else if (playerRigidbody2D.velocity.x > 0f)
+            playerSpriteRenderer.flipX = false;
+
+        if(cameraTransform != null) 
+        {
+            cameraTransform.position = new Vector3(
+                transform.position.x,
+                transform.position.y,
+                cameraTransform.position.z
+                );
+        }
+    }
+
     private void FixedUpdate()
     {
         
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        isGround = Physics2D.OverlapCircle(bottomCollisionPoint.position, .2f, tileLayer);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) 
+    {
+        isGround = false;
     }
 }
